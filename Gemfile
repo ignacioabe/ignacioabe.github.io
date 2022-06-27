e927023cae354ccfeb9e953329b65a55062ebd18
ignacioabe <ignacio.abe@gmail.com>
2022-06-27 12:38:38 +0200

    añade tailwind a jekyll con postcss

--- Gemfile
+++ Gemfile
@@ -6,6 +6,6 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # gem "rails"
 
-gem "jekyll", "~> 4.0"
-
-gem "webrick", "~> 1.7"
+gem "jekyll"
+gem "webrick"
+gem "jekyll-postcss"
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    './_drafts/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_proyectos/**/*.html',
+    './_posts/*.md',
+    './*.md',
+    './*.html',
+  ],
+  theme: {
+      extend: {},
+      fontFamily: {
+        'sans': ['"Work Sans"', "sans-serif"],
+        // 'serif': ['ui-serif', 'Georgia', ...],
+        // 'mono': ['"Space Mono"', "monospace"],
+        // 'display': ['Oswald', ...],
+        // 'body': ['"Open Sans"', ...],
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ]
+}
