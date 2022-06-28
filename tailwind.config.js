11a531b17aad48b23f221b5bf8517e3214579e9d
ignacioabe <ignacio.abe@gmail.com>
2022-06-28 11:18:30 +0200

    termina solución parche

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_drafts/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_proyectos/**/*.html',
+    './_posts/*.md',
+    './*.md',
+    './*.html',
+  ],
+  theme: {
+      extend: {},
+      fontFamily: {
+        'sans': ['"Work Sans"', "sans-serif"],
+        // 'serif': ['ui-serif', 'Georgia', ...],
+        // 'mono': ['"Space Mono"', "monospace"],
+        // 'display': ['Oswald', ...],
+        // 'body': ['"Open Sans"', ...],
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ]
+}
